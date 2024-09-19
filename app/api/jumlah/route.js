89bfc8ee56185782581886256c30d8d6d1336f5f
bugs macbook-air <rnaldi491@gmail.com>
2024-09-20 07:16:20 +0900

    gyt

--- app/api/jumlah/route.js
+++ app/api/jumlah/route.js
@@ -2,8 +2,11 @@ import { prisma } from "@/app/lib/db"
 import { NextResponse } from "next/server"
 
 export const GET = async (request) => {
-    const jumlahKontak = await prisma.kontak.findMany()
-    console.log("Jumlah kontak", jumlahKontak.length)
-    const jumlah = jumlahKontak.length
-    return NextResponse.json({"jumlah":jumlah}, {status:200})
+    try {
+        const jumlahKontak = await prisma.kontak.count()
+        return NextResponse.json({"jumlah":jumlahKontak}, {status:200})
+    } catch (error) {
+        return NextResponse.json({"jumlah":"tidak ada data"}, {status:500})
+    }
+    
 }
