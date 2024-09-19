159db2a3564cebab6414f4716045c515ef48445e
bugs macbook-air <rnaldi491@gmail.com>
2024-09-20 07:40:12 +0900

    new update

--- app/api/jumlah/route.js
+++ app/api/jumlah/route.js
@@ -2,6 +2,8 @@ import { prisma } from "@/app/lib/db"
 import { NextResponse } from "next/server"
 
 export const GET = async (request) => {
+    const { searchParams } = new URL(request.url);
+    const forceRefresh = searchParams.get('forceRefresh');
     try {
         const jumlahKontak = await prisma.kontak.count()
         console.log(jumlahKontak)
